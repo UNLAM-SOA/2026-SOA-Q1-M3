950268024a02cd4b9e8d49ef41062c080ba786f5
Lucia Castiñeiras <castineiraslucia1@gmail.com>
2026-06-29 20:57:48 -0300

    Eliminamos lo que tenia que ver con comandos ya que lo hacemos con mqtt

--- Node-red/mysql/init.sql
+++ Node-red/mysql/init.sql
@@ -5,12 +5,3 @@ CREATE TABLE IF NOT EXISTS telemetry (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_device_created (device_id, created_at)
 );
-
-CREATE TABLE IF NOT EXISTS commands (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  device_id VARCHAR(100) NOT NULL,
-  command_text VARCHAR(255) NOT NULL,
-  origin VARCHAR(50) DEFAULT 'app',
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  INDEX idx_device_created (device_id, created_at)
-);
